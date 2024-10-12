99f804761983ae2340c2ed41454141c2b972559c
EricB <eric.batsaikhan2@gmail.com>
2024-10-12 19:47:54 -0400

    Created first spring RestController class.

---
+++ src/test/java/com/ericb/movieWatchList/MovieWatchListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericb.movieWatchList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWatchListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
